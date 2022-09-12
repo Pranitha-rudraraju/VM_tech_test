e9721d2ed8fad50bce7e9472b24d66c70736158b
pranitha rudraraju <113337815+Pranitha-rudraraju@users.noreply.github.com>
2022-09-12 18:44:52 +0100

    updating notes
    updating notes

--- composite_transform.py
+++ composite_transform.py
@@ -10,7 +10,7 @@ class ProcessData(beam.PTransform):
 def run():    
     with beam.Pipeline() as pipeline:
       #data = beam.io.ReadFromText("gs://cloud-samples-data/bigquery/sample-transactions/transactions.csv")
-      data = pipeline | "Read data from GCS" >> beam.io.ReadFromText("transactions.csv", skip_header_lines=1)
+      data = pipeline | "Read data from GCS" >> beam.io.ReadFromText("transactions.csv", skip_header_lines=1) # # sample data downloaded and saved in the local file (transactions.txt) to read as i was not able to do on work pc due to networking issue. 
       transformed_data = data | ProcessData()
       output = transformed_data | beam.io.WriteToText('output/composite_transform_results', file_name_suffix= '.csv', header = 'date, total_amount') #| beam.Map(print)      
 
